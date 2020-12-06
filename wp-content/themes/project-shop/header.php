77d3f70c57e0010de29d283c7dba7ce87c254226
viktorosvoll <viktor.osvoll@gmail.com>
2020-12-06 15:29:27 +0100

    added markup and enqueued css

--- wp-content/themes/project-shop/header.php
+++ wp-content/themes/project-shop/header.php
@@ -4,5 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
+
+    <?php wp_head(); ?>
+
 </head>
-<body>
+<body <?php body_class(); ?>>
