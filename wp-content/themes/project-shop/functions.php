54f3557b25cda67f3341fbeb168b8f6e632a5d9d
viktorosvoll <viktor.osvoll@gmail.com>
2020-12-06 15:48:01 +0100

    enqueued sass and installed node

--- wp-content/themes/project-shop/functions.php
+++ wp-content/themes/project-shop/functions.php
@@ -2,7 +2,16 @@
 
 
 function load_stylesheets(){
-    wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', '', 1, 'all');
+    wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', '', 1, 'all'); // file, dependencies, version, devices
     wp_enqueue_style('stylesheet');
+
+    wp_register_style('custom', get_template_directory_uri() . '/app.css', '', 1, 'all'); // file, dependencies, version, devices
+    wp_enqueue_style('custom');
 }
-add_action('wp_enqueue_scripts', 'load_stylesheets');
+add_action('wp_enqueue_scripts', 'load_stylesheets'); 
+
+function load_javascript(){
+    wp_register_script('custom', get_template_directory_uri() . '/app.js', '', 1, true); // file, dependencies, version, devices
+    wp_enqueue_script('custom');
+}
+add_action('wp_enqueue_scripts', 'load_javascript');
